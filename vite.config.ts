547fbd7a13fdf5e886214d047e8a9317aba8d5a0
dhnho <huunhodc4869@gmail.com>
2025-05-06 22:36:51 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()]
+  plugins: [react(), mkcert()],
+  base: '/'
 })
